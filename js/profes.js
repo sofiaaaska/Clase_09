bdd32480c202b2cd49aed5f902cf1d673b3cedb4
Sofía <sofia.verdugo@ug.uchile.cl>
2025-05-13 00:03:18 -0400

    Add files via upload

--- js/profes.js
+++ js/profes.js
@@ -497,7 +497,7 @@ async function datos(criterio) {
     }
 }
 
-datos("Abud Carrillo, Jenny").catch((error) => console.error(error));
+datos("Gatica Ramírez, Pamela").catch((error) => console.error(error));
 
 document.querySelectorAll("select")[0].addEventListener("change", (event) => {
     tabla.innerHTML = " ";
